929e5cd8637935d9a2c569e60bea57a7fcd82245
ximing <ximing@meituan.com>
2017-03-30 11:44:52 +0800

    update quill

--- src/lib/quillEditor.js
+++ src/lib/quillEditor.js
@@ -89,6 +89,8 @@ export const initQuillEditor = function (dom, options) {
                     // Object.keys(format.currentFormat).forEach(item=>{
                     //
                     // })
+                } else {
+                    console.log('Cursor not in the editor');
                 }
             }
         }
