d5ad67257754546c600f8317f077fe0a01d8f486
ximing <ximing@meituan.com>
2017-07-19 11:31:26 +0800

    support lineheight

--- src/lib/initQuill.js
+++ src/lib/initQuill.js
@@ -5,9 +5,8 @@
 import Quill from 'quill';
 import Delta from 'quill-delta';
 import {ImageResize} from './quill-image-resize-module';
-// import { ImageDrop } from './quill-image-drop-module';
-// Quill.register('modules/imageDrop', ImageDrop);
-Quill.register('modules/imageResize', ImageResize);
+import {LineHeightStyle, LineHeightAttribute} from './format/lineHeight';
+
 var SizeStyle = Quill.import('attributors/style/size');
 var ColorStyle = Quill.import('attributors/style/color');
 var BackgroundStyle = Quill.import('attributors/style/background');
@@ -15,17 +14,29 @@ var AlignStyle = Quill.import('attributors/style/align');
 
 SizeStyle.whitelist = [];
 ColorStyle.whitelist = null;
-BackgroundStyle.whitelist = ['yellow','green','cyan','magenta','darkYellow','darkyellow','darkGray','darkgray','lightGray','lightgray','black',
-    'blue','red','darkBlue','darkblue','darkCyan','darkcyan','darkGreen','darkgreen','darkMagenta','darkmagenta','darkRed','darkred','white'];
-AlignStyle.whitelist = ['right', 'center', 'justify','left'];
+BackgroundStyle.whitelist = [
+    'yellow', 'green', 'cyan', 'magenta', 'darkYellow', 'darkyellow',
+    'darkGray', 'darkgray', 'lightGray', 'lightgray', 'black',
+    'blue', 'red', 'darkBlue', 'darkblue', 'darkCyan', 'darkcyan',
+    'darkGreen', 'darkgreen', 'darkMagenta', 'darkmagenta', 'darkRed',
+    'darkred', 'white'];
+AlignStyle.whitelist = ['right', 'center', 'justify', 'left'];
+
 for (let i = 5; i <= 72; i++) {
     SizeStyle.whitelist.push(`${i}pt`);
     SizeStyle.whitelist.push(`${i}.5pt`);
 }
+
 Quill.register(SizeStyle, true);
 Quill.register(ColorStyle, true);
 Quill.register(BackgroundStyle, true);
 Quill.register(AlignStyle, true);
+Quill.register({
+    'modules/imageResize': ImageResize,
+    // 'formats/lineheight': LineHeightStyle
+}, true);
+Quill.register(LineHeightAttribute);
+Quill.register(LineHeightStyle);
 
 var Clipboard = Quill.import('modules/clipboard');
 class PlainClipboard extends Clipboard {
