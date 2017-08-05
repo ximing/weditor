d230daadeac7779dca180385f021e922ff7b52a5
席铭 <ximing@meitan.com>
2017-08-05 23:04:10 +0800

    fix https://github.com/quilljs/quill/issues/1374

--- src/lib/initQuill.js
+++ src/lib/initQuill.js
@@ -50,5 +50,24 @@ class PlainClipboard extends Clipboard {
         this.container.innerHTML = '';
         return new Delta().insert(text);
     }
+
+    onPaste(e) {
+    if (e.defaultPrevented || !this.quill.isEnabled()) return;
+    let range = this.quill.getSelection();
+    let delta = new Delta().retain(range.index);
+    let scrollTop = this.quill.scrollingContainer.scrollTop;
+    this.container.style.position = 'fixed';
+    this.container.style.zIndex = '-1';
+    this.container.focus();
+    this.quill.selection.update(Quill.sources.SILENT);
+    setTimeout(() => {
+      delta = delta.concat(this.convert()).delete(range.length);
+      this.quill.updateContents(delta, Quill.sources.USER);
+      // range.length contributes to delta.length()
+      this.quill.setSelection(delta.length() - range.length, Quill.sources.SILENT);
+      this.quill.scrollingContainer.scrollTop = scrollTop;
+      this.quill.focus();
+    }, 1);
+  }
 };
 Quill.register('modules/clipboard', PlainClipboard, true);
